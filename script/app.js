05f3866b35dd3cbf927332a4a57a66d351c7dc72
kamaeff <kamaeff2@gmail.com>
2022-08-21 14:36:42 +0300

    Сделан адаптив для header и верхней части main, переделана структура css по codeacademy, переделаны названия классов для header и main

--- script/app.js
+++ script/app.js
@@ -1,22 +1,47 @@
-const mainMenu = document.querySelector('.header__mainMenu');
-const closeMenu = document.querySelector('.closeMenu');
-const openMenu = document.querySelector('.header__openMenu');
-const menu_items = document.querySelectorAll('nav .header__mainMenu li a');
+const mainMenu = document.querySelector(".header__main-menu");
+const closeMenu = document.querySelector(".header__close-menu");
+const openMenu = document.querySelector(".header__mobile-menu");
+const menu_items = document.querySelectorAll("nav .header__main-menu li a");
 
-openMenu.addEventListener('click',show);
-closeMenu.addEventListener('click',close);
+openMenu.addEventListener("click", show);
+closeMenu.addEventListener("click", close);
 
-// close menu when you click on a menu item 
-menu_items.forEach(item => {
-    item.addEventListener('click',function(){
-        close();
-    })
-})
+// close menu when you click on a menu item
+menu_items.forEach((item) => {
+  item.addEventListener("click", function () {
+    close();
+  });
+});
 
-function show(){
-    mainMenu.style.display = 'flex';
-    mainMenu.style.top = '0';
+function show() {
+  mainMenu.style.display = "flex";
+  mainMenu.style.top = "0";
 }
-function close(){
-    mainMenu.style.top = '-100%';
+function close() {
+  mainMenu.style.top = "-100%";
 }
+
+const swiper = new Swiper(".swiper-container", {
+  spaceBetween: 10,
+  loop: true,
+
+
+  //Autoplay
+  autoplay: {
+    delay: 3000,
+  },
+
+  //Navigation
+  navigation: {
+    clickable: true,
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  //Scrollbar
+  scrollbar: {
+    el: ".swiper-scrollbar",
+    draggable: true,
+  },
+
+  mousewheel: true,
+});
