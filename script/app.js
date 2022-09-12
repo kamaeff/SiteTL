a7e575ed3238266e83224bda31e2681ed5146050
kamaeff <kamaeff2@gmail.com>
2022-09-12 18:05:04 +0300

    Переделаны стрелки слайдера + переделаны названия классов

--- script/app.js
+++ script/app.js
@@ -6,13 +6,6 @@ const menu_items = document.querySelectorAll("nav .header__main-menu li a");
 openMenu.addEventListener("click", show);
 closeMenu.addEventListener("click", close);
 
-// close menu when you click on a menu item
-menu_items.forEach((item) => {
-  item.addEventListener("click", function () {
-    close();
-  });
-});
-
 function show() {
   mainMenu.style.display = "flex";
   mainMenu.style.top = "0";
@@ -20,6 +13,12 @@ function show() {
 function close() {
   mainMenu.style.top = "-100%";
 }
+
+menu_items.forEach((item) => {
+  item.addEventListener("click", function () {
+    close();
+  });
+});
 
 const swiper = new Swiper(".swiper-container", {
   spaceBetween: 10,
